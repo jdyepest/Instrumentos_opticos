dd49b63c96ba28421c5e646d8defeb8039454b1b
Juan David <jdavidth.01@gmail.com>
2021-11-05 16:42:48 -0500

    fourth commit

--- WhereIsWaldo.py
+++ WhereIsWaldo.py
@@ -64,36 +64,58 @@ def paddin(img,img1):
     x_center = (new_image_width - old_image_width) // 2
     y_center = (new_image_height - old_image_height) // 2
     # copy img image into center of result image
-    result[y_center:y_center+old_image_height, 
-    x_center:x_center+old_image_width] = img
+    #result[y_center:y_center+old_image_height, 
+    #x_center:x_center+old_image_width] = img
+
+    result[0:0+old_image_height, 
+    0:0+old_image_width] = img
 
     return result
 
+def mask(img, center=None, radius=None):
+    h = img.shape[0]
+    w = img.shape[1]
+
+    if center is None: # use the middle of the image
+        center = (int(w/2), int(h/2))
+    if radius is None: # use the smallest distance between the center and image walls
+        radius = min(center[0], center[1], w-center[0], h-center[1])
+
+    Y, X = np.ogrid[:h, :w]
+    dist_from_center = np.sqrt((X - center[0])**2 + (Y-center[1])**2)
+
+    mask = dist_from_center <= radius / 10
+    return mask
+
 
 imagen = cv2.imread('c.jpg', cv2.IMREAD_UNCHANGED)
+imagen = cv2.cvtColor(imagen,cv2.COLOR_BGR2RGB)
 imagen_gris = cv2.cvtColor(imagen, cv2.COLOR_BGR2GRAY)
 #imagen_gris = cv2.rotate(imagen_gris, cv2.ROTATE_180)
 
 pista = cv2.imread('c_clue.jpg', cv2.IMREAD_UNCHANGED)
-pista_gris = cv2.cvtColor(pista, cv2.COLOR_BGR2GRAY)
+pista = cv2.cvtColor(pista,cv2.COLOR_BGR2RGB)
+pista_gris = cv2.cvtColor(pista, cv2.COLOR_RGB2GRAY)
 pista_gris_padded = paddin(pista_gris, imagen_gris)
+#pista_gris_padded = cv2.rotate(pista_gris_padded, cv2.ROTATE_180)
 
 test1 = cv2.imread('E.png', cv2.IMREAD_UNCHANGED)
 test1_gris = cv2.cvtColor(test1, cv2.COLOR_BGR2GRAY)
 
 
-test2 = cv2.imread('letters.jpg', cv2.IMREAD_UNCHANGED)
-test2_gris = cv2.cvtColor(test2, cv2.COLOR_BGR2GRAY)
+test2 = cv2.imread('b.png', cv2.IMREAD_UNCHANGED)
+#test2_gris = cv2.cvtColor(test2, cv2.COLOR_BGR2GRAY)
+
 
 
 fft_pista = transformada_fourier(pista_gris_padded)
 fft_imagen = transformada_fourier(imagen_gris)
 fft_test1 = transformada_fourier(test1_gris)
-fft_test2 = transformada_fourier(test2_gris)
+fft_test2 = transformada_fourier(test2)
+
+res = fft_imagen * fft_pista
+ires = transformada_fourier_inversa(res)
+im = modulo_cuadrado_log(ires)
 
 
-#TF = (fft_imagen * fft_pista)
-#ITF = transformada_fourier_inversa(TF)
-#graficar_modulo_cuadrado (fft_pista)
-
-plot_func(imagen_gris,modulo_cuadrado_log(fft_imagen))
+plot_func(modulo_cuadrado_log(fft_imagen),imagen_gris)
