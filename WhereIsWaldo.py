efae1800a76c7aca8c2b3a30d69139c04e7185ad
Juan David <jdavidth.01@gmail.com>
2021-11-04 23:25:04 -0500

    second commit

--- WhereIsWaldo.py
+++ WhereIsWaldo.py
@@ -1,5 +1,18 @@
 import numpy as np
-import matplotlib as plt
 import cv2
+import matplotlib.pyplot as plt
 
-print(2+3)
+nm = 1e-9 #sufijo para especificar que una cantidad esta en nanometros
+um = 1e-6 #sufijo para especificar que una cantidad esta en micrometros
+mm = 1e-3 #sufijo para especificar que una cantidad esta en milimetros
+
+imagen = cv2.imread('c.jpg')
+imagen_gris = cv2.cvtColor(imagen, cv2.COLOR_BGR2GRAY)
+
+pista = cv2.imread('c_clue.jpg')
+pista_gris = cv2.cvtColor(pista, cv2.COLOR_BGR2GRAY)
+
+cv2.imshow('df',imagen_gris)
+cv2.waitKey()
+
+
